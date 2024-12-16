b061fd1c1f52f11cdfb6ee7798a36e310a9831ae
Salyam <sallamyemen@hotmail.com>
2024-12-16 22:10:02 +0300

    Update catalogue_menu.php

--- catalogue_menu.php
+++ catalogue_menu.php
@@ -2,7 +2,6 @@
 
 function checkSlug($val)
 {
-
         $slug = explode('/', Yii::$app->request->get('slug'));
         $cnt = 0 ;
         foreach($slug as $item)
@@ -20,16 +19,12 @@ function checkSlug($val)
         if($item == $val)
             return true;
 }
-
-
-
 ?>
 
 <?php function openTree($children, $depth) { ?>
     <ul class="<?= $depth == 1 ? 'lv-root' : '' ?> <?= $depth <= 1 ? 'side-menu' : 'side-sub-menu' ?> <?= $depth == 2 ? 'li-active' : '' ?> <?= $depth == 3 ? 'depth' : '' ?> <?= $depth > 3 ? 'deeper' : '' ?>">
     <?php foreach($children as $item) : ?>
                 <li>
-
                 <?php $ankor = $item['ankor'] ? $item['ankor'] : $item['title']; ?>
 
                 <?php if($item['routs']['route'] == Yii::$app->request->get('slug')) : ?>
@@ -41,7 +36,6 @@ function checkSlug($val)
                         <?= openTree($item['children'], $item['depth']); ?>
                     <?php endif ?>
                 </li>
-
 
     <?php endforeach ?>
     </ul>
